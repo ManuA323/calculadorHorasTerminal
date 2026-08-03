38ecf25420d297ef2c737f300bcd428c7bd31d89
ManuelAgreda <magreda@undav.edu.ar>
2026-08-03 11:55:34 -0300

    Corregir instalador

--- install.sh
+++ install.sh
@@ -5,12 +5,7 @@ REPO="https://raw.githubusercontent.com/ManuA323/calculadorHorasTerminal/main"
 
 echo "Descargando calculador..."
 
-sudo mkdir -p /usr/local/lib
-
-sudo curl -fsSL "$REPO/trabajo.awk" -o /usr/local/lib/trabajo.awk
-sudo chmod +x /usr/local/lib/trabajo.awk
-
-sudo curl -fsSL "$REPO/trabajo" -o /usr/local/bin/trabajo
+sudo curl -fsSL "$REPO/trabajo.awk" -o /usr/local/bin/trabajo
 sudo chmod +x /usr/local/bin/trabajo
 
 echo
@@ -18,4 +13,4 @@ echo "Instalado correctamente."
 echo
 echo "Usá:"
 echo
-echo "    trabajo"
+echo "trabajo"
