630532055bab92bdd7820f523a087a2c01938f67
ManuelAgreda <magreda@undav.edu.ar>
2026-08-25 12:12:35 -0300

    cambio de directorio de registros a una ruta absoluta.

--- install.sh
+++ install.sh
@@ -5,21 +5,26 @@ REPO="https://raw.githubusercontent.com/ManuA323/calculadorHorasTerminal/main"
 
 echo "Instalando calculador..."
 
+# Crear directorio de librerías del sistema
 sudo mkdir -p /usr/local/lib
 
+# Descargar script AWK
 sudo curl -fsSL "$REPO/trabajo.awk" \
     -o /usr/local/lib/trabajo.awk
 
 sudo chmod +x /usr/local/lib/trabajo.awk
 
-
+# Descargar ejecutable bash
 sudo curl -fsSL "$REPO/horas" \
     -o /usr/local/bin/horas
 
 sudo chmod +x /usr/local/bin/horas
 
+# Crear directorio global para los datos y asignar permisos de lectura/escritura a todos los usuarios
+sudo mkdir -p /var/lib/horas
+sudo chmod 777 /var/lib/horas
 
 echo
-echo "Instalacion completa."
+echo "Instalación completa."
 echo
 echo "Ejecuta el comando \"horas\" para calcular tus horas de trabajo."
